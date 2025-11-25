385ac1bcba92ef8735339216c870e19bcd71e732
ArBes <arina.arifovnaa@gmail.com>
2025-11-25 15:20:50 +0300

    Класс кодов кнопок

---
+++ src/presentation/static.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+class Keys(Enum):
+    W_UP = (87, 119)  # W и w
+    A_LEFT = (65, 97)  # A и a
+    S_DOWN = (83, 115)  # S и s
+    D_RIGHT = (68, 100)  # D и d
+
+    H_USE_WEAPON = (72, 104)  # H и h
+    J_USE_FOOD = (74, 106)  # J и j
+    K_USE_ELIXIR = (75, 107)  # K и k
+    E_USE_SCROLL = (69, 101) # E и e
